9f58c37592f0a57e1d2cb99d8f3a9b1569ddcc21
datewat <thomaszimmer524@gmail.com>
2022-05-22 18:59:36 +0200

    moved past sessions to seperate directory

--- session.sh
+++ session.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 d=`date +%Y-%m-%d-%H-%M` 
 
+mkdir -p past_sessions
 mkdir $d
 python3 database.py
 cp database.py $d
@@ -10,8 +11,8 @@ mv user.db $d
 cp index.html $d
 #echo  $d >> verlauf.txt
 cd $d
-mkdir user_image
-mkdir game_image
-touch config.txt
+mkdir $d/user_image
+mkdir $d/game_image
+touch $d/config.txt
 
 exit -1
