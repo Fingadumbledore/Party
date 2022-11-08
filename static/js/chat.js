6186f54f321d0539dc6a9720a4a53d04284b682a
hyperbel <thomaszimmer524@gmail.com>
2022-11-08 18:04:56 +0100

    onload window execute stuff

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,14 +1,16 @@
-const username = document.getElementById('userNameEntry').innerHTML;
-const ws = new WebSocket('ws://localhost:5000')
-ws.addEventListener('open', (e) => {
-  ws.send(JSON.stringify({
-    'user': 'test'
-  }))
-})
+window.onload = function() {
+  const username = document.getElementById('userNameEntry').innerHTML;
+  const ws = new WebSocket('ws://localhost:5000')
+  ws.addEventListener('open', (e) => {
+    ws.send(JSON.stringify({
+      'user': 'test'
+    }))
+  })
 
-function join_chat() {
-  ws.send(JSON.stringify({
-    'msg_type': 'user_join',
-    'username': username
-  }))
+  function join_chat() {
+    ws.send(JSON.stringify({
+      'msg_type': 'user_join',
+      'username': username
+    }))
+  } 
 }
