475b31fc099141dcb4ea1002203aa9187045d98f
hyperbel <thomaszimmer524@gmail.com>
2022-11-23 18:33:17 +0100

    check checksum on run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,13 @@
 python -c "import qrcode"
 if [ $? -eq 1 ]; then
 	pip install qrcode
-else
-cat party.sql | sqlite3 party.db
+fi
+
+md5sum -c .sums
+if [ $? -eq 1 ]; then
+	rm party.db
+	cat party.sql | sqlite3 party.db
+fi
 
 export FLASK_APP=main.py
 
