24532ce6d4161ce86df2180af1103af8b2f3319b
hyperbel <thomaszimmer524@gmail.com>
2022-11-23 18:23:03 +0100

    minor fixes

--- run.sh
+++ run.sh
@@ -1,13 +1,11 @@
+#!/bin/sh
 
 python -c "import qrcode"
 if [ $? -eq 1 ]; then
 	pip install qrcode
 else
-	echo "qrcode package is installed"
-fi
 cat party.sql | sqlite3 party.db
 
 export FLASK_APP=main.py
 
-
- flask run
+flask run
