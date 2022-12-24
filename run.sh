25eeb333bc39200e92b8a9f646f5d04043064c54
fingadumbledore <fingadumbledore@gmail.com>
2022-12-24 13:29:21 +0100

    automatische IDS

--- run.sh
+++ run.sh
@@ -5,11 +5,8 @@ if [ $? -eq 1 ]; then
 	pip install qrcode
 fi
 
-md5sum -c .sums
-if [ $? -eq 1 ]; then
-	rm party.db
-	cat party.sql | sqlite3 party.db
-fi
+
+cat party.sql | sqlite3 party.db
 
 export FLASK_APP=main.py
 
