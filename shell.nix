daa24581fde289f8d384cc26567f3e718735d803
Hyperbel <25338593+hyperbel@users.noreply.github.com>
2022-12-25 17:23:07 +0100

    reproducable env with nix

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-22.11.tar.gz") {} }:
+pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (ps: [
+      ps.flask
+      ps.qrcode
+    ]))
+
+    pkgs.curl
+    pkgs.vim
+    pkgs.sqlite
+    pkgs.tmux
+  ];
+}
