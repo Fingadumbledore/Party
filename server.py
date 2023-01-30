cbc48d2c545fc02904c7c934392ece01dc8bfa80
fingadumbledore <fingadumbledore@gmail.com>
2023-01-30 07:58:37 +0100

    chat update

--- server.py
+++ server.py
@@ -657,6 +657,33 @@ def server():
             return "File uploaded successfully!"
         return "No file found"
 
+    @app.route("/send", methods=["POST"])
+    def send():
+        message = request.form["message"]
+        username = request.form["username"]
+        
+        conn = sqlite3.connect("chat.db")
+        c = conn.cursor()
+        c.execute("INSERT INTO messages (username, message) VALUES (?, ?)", (username, message))
+        conn.commit()
+        conn.close()
+        
+        return render_template("index.html")
+
+    @app.route("/get")
+    def get():
+        conn = sqlite3.connect("chat.db")
+        c = conn.cursor()
+        c.execute("SELECT username, message, timestamp FROM messages")
+        messages = c.fetchall()
+        conn.close()
+        
+        return render_template("messages.html", messages=messages)
+
+    @app.route("/chat")
+    def chat():
+        return render_template("chat.html")
+
     @app.errorhandler(404)
     def page_not_found(e):
         error_log("called non-existing page")
--- static/js/chat.js
+++ static/js/chat.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+/*window.onload = function() {
   const username = document.getElementById('userNameEntry').innerHTML;
   const ws = new WebSocket('ws://localhost:5000')
   ws.addEventListener('open', (e) => {
@@ -13,4 +13,62 @@ window.onload = function() {
       'username': username
     }))
   } 
+}*/
+
+<script>
+let unread = 0;
+
+function updateTitle() {
+  document.title = "Chat (" + unread + ")";
 }
+
+function addMessage(username, message, timestamp) {
+  let messagesDiv = document.getElementById("messages");
+  let messageDiv = document.createElement("div");
+  messageDiv.classList.add("message");
+  let usernameSpan = document.createElement("span");
+  usernameSpan.classList.add("username");
+  usernameSpan.innerText = username;
+  let messageSpan = document.createElement("span");
+  messageSpan.innerText = message.substring(0, 80);
+  let timestampSpan = document.createElement("span");
+  timestampSpan.classList.add("timestamp");
+  timestampSpan.innerText = timestamp;
+  messageDiv.appendChild(usernameSpan);
+  messageDiv.appendChild(document.createTextNode(": "));
+  messageDiv.appendChild(messageSpan);
+  messageDiv.appendChild(document.createElement("br"));
+  messageDiv.appendChild(timestampSpan);
+  messagesDiv.appendChild(messageDiv);
+  messagesDiv.scrollTop = messagesDiv.scrollHeight;
+  unread += 1;
+  updateTitle();
+}
+
+let inputField = document.getElementById("input");
+inputField.addEventListener("input", function() {
+  let value = inputField.value;
+  if (value.length > 80) {
+    inputField.value = value.substring(0, 80);
+  }
+});
+
+function getMessages() {
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "/get");
+xhr.onload = function() {
+  document.getElementById("messages").innerHTML = xhr.responseText;
+};
+xhr.send();
+}
+
+function sendMessage() {
+var message = document.getElementById("message").value;
+var username = document.getElementById("username").value;
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", "/send");
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+xhr.send("message=" + message + "&username=" + username);
+}
+</script>
