674033c53f41ad635c5cb86eb247540911f1d80c
hyperbel <hyperbel@users.noreply.github.com>
2023-05-19 20:04:03 +0200

    cleaned log

--- log.py
+++ log.py
@@ -12,31 +12,11 @@ def clear_log():
 def log_server(log, type):
     """ server log""" 
     raise Exception("Deprecated, use insert_log()")
-    file_size = os.path.getsize('./Config/log/server.log')
-    date = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(time.time()))
-    if file_size >= 1024*1024:
-        clear_log()
-    log = date + ' [' + type + '] ' + log
-    datei = open('./Config/log/server.log', 'a')
-    datei.write('\n' + " " + log)
-    log = date
-    datei.close()
-        
 
 def chat_log(data, log_type):
     """ chat log """
 
     raise Exception("Deprecated, use insert_log()")
-    file_size = os.path.getsize('./Config/log/chat.log')
-    date = lambda: time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(time.time()))
-    if file_size >= 1024*1024:
-        clear_log()
-    current_date = date()
-    data = f'{current_date} [{log_type}]  {data}\n'
-    datei = open('./Config/log/chat.log', 'a')
-    datei.write(data)
-    data = date()
-    datei.close()
 
 def insert_log(file: str, data: str, log_type: str):
     full_file_name = f'./Config/log/{file}.log'
