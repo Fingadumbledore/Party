a7a6451506484a5d0537dde2baab5c1f3daad824
hyperbel <hyperbel@users.noreply.github.com>
2023-07-05 15:50:42 +0200

    mate kiste tests

--- tests/test_mate.py
+++ tests/test_mate.py
@@ -1,4 +1,5 @@
-from tests import client, app
+from tests import client, app, mateKiste
+from party import MateMarke
 
 class TestMate:
     def test_api_mate(self, client):
@@ -12,3 +13,16 @@ class TestMate:
     def test_api_mate_trinken(self, client):
         response = client.post("/api/mate/trinken")
         assert response.status_code == 200
+
+    def test_eineTrinken(self, mateKiste):
+        mateKiste.eineTrinken()
+        assert mateKiste.getFlaschenAnzahl() == 19
+
+    def test_getFlaschenAnzahl(self, mateKiste):
+        assert mateKiste.getFlaschenAnzahl() == 20
+
+    def test_getMarke(self, mateKiste):
+        assert mateKiste.getMarke() == MateMarke.ClubMate
+
+    def test_getMarkeName(self, mateKiste):
+        assert mateKiste.getMarkeName() == "Club Mate"
