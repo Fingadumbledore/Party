ef3b45f8b7c84a79b5640662ee4aa2993d04e0c3
hyperbel <hyperbel@users.noreply.github.com>
2023-07-05 02:47:14 +0200

    flake8 stuff

--- party/main.py
+++ party/main.py
@@ -1,4 +1,5 @@
 from party import create_app
 
+
 if __name__ == '__main__':
     create_app().run(debug=True, host='localhost', port=5000)
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,15 +1,18 @@
 import pytest
 from party import create_app
 
+
 @pytest.fixture
 def app():
     app = create_app()
     yield app
 
+
 @pytest.fixture
 def client(app):
     client = app.test_client()
     yield client
+
 
 @pytest.fixture
 def runner(app):
