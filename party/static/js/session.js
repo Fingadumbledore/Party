ed8dddfb6b7a7330c704b4ab81e8c1ba8514a8b8
fingadumbledore <fingadumbledore@gmail.com>
2023-07-10 11:55:04 +0200

    es wird gespeichert wo man sich befindet

--- party/static/js/session.js
+++ party/static/js/session.js
@@ -6,48 +6,47 @@ window.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-// Array zum Speichern der Div-Elemente
-var divs = document.querySelectorAll('.hidden-div');
-// Aktuell ausgewählter Button-Index
-var activeButtonIndex = null;
-
-// Funktion zum Umschalten der Div-Elemente
-function toggleDiv(index) {
-    // Button-Status aktualisieren
+  var divs = document.querySelectorAll('.hidden-div');
+  var activeButtonIndex = null;
+  
+  // Beim Laden der Seite den Zustand wiederherstellen
+  window.addEventListener('DOMContentLoaded', function() {
+    var storedIndex = sessionStorage.getItem('position');
+    if (storedIndex) {
+      activeButtonIndex = parseInt(storedIndex);
+      toggleDiv(activeButtonIndex);
+    }
+  });
+  
+  function toggleDiv(index) {
     updateButtonStatus(index);
-
-    // Alle Div-Elemente ausblenden
     hideAllDivs();
-
-    // Index des gewählten Div-Elements
+    
     var selectedIndex = index - 1;
-
     handleSwitchDiv(index);
-
-    // Gewähltes Div-Element einblenden
+    
     divs[selectedIndex].style.display = 'block';
-}
-
-// Funktion zum Ausblenden aller Div-Elemente
-function hideAllDivs() {
+  
+    // Index im Session Storage speichern
+    sessionStorage.setItem('position', index);
+  }
+  
+  function hideAllDivs() {
     for (var i = 0; i < divs.length; i++) {
-        divs[i].style.display = 'none';
+      divs[i].style.display = 'none';
     }
-}
-
-// Funktion zum Aktualisieren des Button-Status
-function updateButtonStatus(index) {
-    // Button-Klassen entfernen
+  }
+  
+  function updateButtonStatus(index) {
     for (var i = 0; i < divs.length; i++) {
-        var button = document.getElementsByClassName('seitenbutton')[i];
-        button.classList.remove('active-button');
+      var button = document.getElementsByClassName('seitenbutton')[i];
+      button.classList.remove('active-button');
     }
-
-    // Aktuellen Button hervorheben
+  
     var activeButton = document.getElementsByClassName('seitenbutton')[index - 1];
     activeButton.classList.add('active-button');
-}
-
+  }
+  
   function togglePopup() {
     var popup = document.getElementById("popup");
     popup.style.display = (popup.style.display === "none") ? "block" : "none";
