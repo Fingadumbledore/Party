1815c2ebfebb361910d8de6693f08222f6ca1d34
fingadumbledore <fingadumbledore@gmail.com>
2023-10-22 14:05:30 +0200

    ein paar weitere änderungen

--- party/static/js/session.js
+++ party/static/js/session.js
@@ -74,3 +74,37 @@ function handleSwitchDiv(index) {
 function skipSong() {}
 function lastSong() {}
 function pausePlaySong() {}
+
+
+function updateClock() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+
+  // Führende Nullen hinzufügen, wenn die Stunden oder Minuten einstellig sind
+  if (hours < 10) {
+      hours = "0" + hours;
+  }
+  if (minutes < 10) {
+      minutes = "0" + minutes;
+  }
+
+  var timeString = hours + ":" + minutes;
+
+  document.getElementById("uhrzeit").innerHTML = timeString;
+}
+
+setInterval(updateClock, 1000);
+
+function showDiv(divId) {
+  const divToShow = document.getElementById(divId);
+  const divs = document.querySelectorAll('div');
+
+  for (const div of divs) {
+      div.classList.add('hidden');
+  }
+
+  divToShow.classList.remove('hidden');
+}
+
+
