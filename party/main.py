2dbdaec1e70018aa1cffd17d3cba2878254b4459
hyperbel <hyperbel@users.noreply.github.com>
2023-10-30 02:09:19 +0100

    set status code directly

--- party/main.py
+++ party/main.py
@@ -20,8 +20,7 @@ STANDARD_ROOM = 0
 
 @app.route('/', methods=['GET'])
 def index():
-    response = jsonify(success=True)
-    response.status_code = HTTPStatus.MOVED_PERMANENTLY
+    response = jsonify(success=True, status_code=HTTPStatus.MOVED_PERMANENTLY)
     if session.get('logged_in'):
         return redirect('/session')
     else:
@@ -30,54 +29,46 @@ def index():
 
 @app.route('/login', methods=['GET'])
 def login():
-    response = jsonify(success=True)
-    response.status_code = 200
+    response = jsonify(success=True, status_code = 200)
     return render_template('login.html')
 
 @app.route('/session', methods=['GET'])
 def omg_i_hate_copilot_session():
-    response = jsonify(success=True)
-    response.status_code = 200
+    response = jsonify(success=True, status_code = 200)
     return render_template('session.html')
 
 @app.route('/api', methods=['GET'])
 def api():
-    response = jsonify(success=True)
-    response.status_code = 200
+    response = jsonify(success=True, status_code = 200)
     return response
 
 @app.route('/api/login', methods=['GET'])
 def api_login():
     session['logged_in'] = True
-    response = jsonify(success=True)
-    response.status_code = HTTPStatus.MOVED_PERMANENTLY
+    response = jsonify(success=True, status_code = HTTPStatus.MOVED_PERMANENTLY)
     return redirect('/session')
 
 
 @app.route('/api/mate', methods=['GET'])
 def api_mate():
-    response = jsonify(success=True)
-    response.status_code = 200
+    response = jsonify(success=True, status_code = 200)
     return response
 
 @app.route('/api/mate/status', methods=['GET'])
 def api_mate_status():
     FLASCHE_COUNT = 20
-    response = jsonify(kiste=MateKiste.getStatus())
-    response.status_code = 200
+    response = jsonify(kiste=MateKiste.getStatus(), status_code = 200)
     return response
 
 @app.route('/api/chat/', methods=['GET'])
 def api_chat():
     messages = Chat.getNextNMessages(160, 0) # 0 offset
-    response = jsonify(success=True, messages=json_util.dumps(messages)) 
-    response.status_code = 200 
+    response = jsonify(success=True, messages=json_util.dumps(messages), status_code = 200)
     return response
 
 @app.route('/api/game/', methods=['GET'])
 def api_game():
-    response = jsonify(success=True)
-    response.status_code = 200
+    response = jsonify(success=True, status_code = 200)
     return response
 
 @app.route('/api/game/point', methods=['GET','POST'])
@@ -94,8 +85,7 @@ def api_game_time():
 
 @app.route('/api/team/', methods=['GET'])
 def api_team():
-    response = jsonify(success=True)
-    response.status_code = 200
+    response = jsonify(success=True, status_code = 200)
     return response
 
 @app.route('/api/team/create', methods=['POST'])
@@ -128,8 +118,7 @@ def handle_chat_get_message(data):
 @app.route('/api/planer', methods=['GET'])
 def api_planer():
     events = Planer.getNextEvents(10, 0)
-    response = jsonify(success=True, events=json_util.dumps(events))
-    response.status_code = 200
+    response = jsonify(success=True, events=json_util.dumps(events), status_code = 200)
     return response
 
 @socketio.on('planer-event')
@@ -169,26 +158,22 @@ def handle_disconnect():
 
 @app.route('/api/music', methods=['GET'])
 def api_music():
-    response = jsonify(success=True)
-    response.status_code = 200
+    response = jsonify(success=True, status_code = 200)
     return response
 
 @app.route('/api/music/skip', methods=['POST'])
 def api_music_skip():
-    response = jsonify(success=True)
-    response.status_code = 200
+    response = jsonify(success=True, status_code = 200)
     return response
 
 @app.route('/api/music/queue', methods=['GET'])
 def api_music_queue():
-    response = jsonify(success=True)
-    response.status_code = 200
+    response = jsonify(success=True, status_code = 200)
     return response
 
 @app.route('/api/music/add_song', methods=['POST'])
 def api_music_add_song():
-    response = jsonify(success=True)
-    response.status_code = 200
+    response = jsonify(success=True, status_code = 200)
     return response
 
 @app.route('/api/event/new', methods=['POST'])
@@ -196,14 +181,12 @@ def api_event_new():
     name = request.form['Event']
     time = request.form['meeting-time']
     print(name, time)
-    response = jsonify(success=True)
-    response.status_code = 200
+    response = jsonify(success=True, status_code = 200)
     return response
 
 @app.errorhandler(404)
 def page_not_found(error):
-    response = jsonify(success=False)
-    response.status_code = 404
+    response = jsonify(success=False, status_code=404)
     return render_template('404.html'), response
 
 if __name__ == '__main__':
@@ -211,5 +194,5 @@ if __name__ == '__main__':
     MateKiste.init()
 
     import logging
-    logging.basicConfig(filename='error.log',level=logging.DEBUG)
+    logging.basicConfig(level=logging.NOTSET, filename='log.log')
     socketio.run(app, debug=True, host='localhost', port=5000) #pragma: no cover
