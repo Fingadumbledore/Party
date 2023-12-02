ca2b22ce8edcd53a367708737cc2114dd6c5082d
fingadumbledore <fingadumbledore@gmail.com>
2023-12-02 08:28:13 +0100

    game anzeige angefangen

--- party/systemMonitor.py
+++ party/systemMonitor.py
@@ -1,18 +1,39 @@
 from pymongo import MongoClient
 from datetime import datetime
 import psutil
-import os
 
 class SystemMonitor:
 
-    CONNECTION_STRING = None
-    client = None
-    collection = None
-    initialized = False
+    CONNECTION_STRING = "mongodb://localhost:27017/"
+    client = MongoClient(CONNECTION_STRING)
+    collection = client['partyyy']['systemMonitor']
+    initialized = True
 
     @classmethod
-    def init(self):
-        self.CONNECTION_STRING = "mongodb://localhost:27017/"
-        self.client = MongoClient(self.CONNECTION_STRING)
-        self.collection = self.client['partyyy']['systemMonitor']
-        self.initialized = True
+    def init(self, cls):
+        if not cls.initialized:
+            self.cls.client = MongoClient(self.cls.CONNECTION_STRING)
+            self.cls.collection = cls.client['partyyy']['systemMonitor']
+            self.cls.initialized = True
+
+    @classmethod
+    def record_system_info(self, cls):
+        if not self.cls.initialized:
+            self.cls.init()
+
+        self.cpu_percent = psutil.cpu_percent(interval=1)
+        self.memory_percent = psutil.virtual_memory().percent
+        self.uptime = int(round(psutil.boot_time()))
+
+        data = {
+            'timestamp': datetime.now(),
+            'cpu_percent': self.cpu_percent,
+            'memory_percent': self.memory_percent,
+            'uptime': self.uptime
+        }
+
+        self.cls.collection.insert_one(data)
+
+    @classmethod
+    def getStatus(self) -> list[bool]:
+        return list(self.record_system_info()['status'])
